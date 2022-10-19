69362c4f39b4416eda2b49b72b83a44f667bbf40
a1a2a3a4a5 <linub7@gmail.com>
2022-10-19 16:42:53 +0330

    add typography - make code cleaner

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,51 +1,19 @@
-import styled from 'styled-components/native';
-import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
-import { Image, Text, View } from 'react-native';
 
 import star from '../../../../assets/icons/star';
 import open from '../../../../assets/icons/open';
 import { Spacer } from '../../../components/spacer/spacer.component';
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
+import { Text } from '../../../components/typography/text.component';
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Address,
+  Icon,
+  Rating,
+  Section,
+  SectionEnd,
+} from './restaurant-info-card.style';
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -66,7 +34,7 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map((_, i) => (
@@ -75,19 +43,13 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: 'red' }}>
-                CLOSED TEMPORARILY
-              </Text>
+              <Text variant="error">CLOSED TEMPORARILY</Text>
             )}
-            {/* <Spacer variant={'left.large'} />
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant={'left.large'} />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} /> */}
             <Spacer position="left" size="large">
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </Spacer>
             <Spacer position="left" size="large">
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
---
+++ src/features/restaurants/components/restaurant-info-card.style.js
@@ -0,0 +1,52 @@
+import { Card } from 'react-native-paper';
+import styled from 'styled-components/native';
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Icon = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Address,
+  Icon,
+  Rating,
+  Section,
+  SectionEnd,
+};
