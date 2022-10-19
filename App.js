4463db971e4443b4ec035cdd0020d0ab9cfbc154
a1a2a3a4a5 <linub7@gmail.com>
2022-10-19 07:36:27 +0330

    add currentHeight status bar for android

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Text>Search</Text>
+      </View>
+      <View style={styles.list}>
+        <Text>list</Text>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    backgroundColor: 'green',
+    height: 50,
+    padding: 16,
+  },
+  list: {
+    backgroundColor: 'blue',
+    flex: 1,
+    padding: 16,
   },
 });
