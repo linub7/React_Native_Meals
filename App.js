786f3d467be5c6cf9f3c0415ff8f266665469ba2
a1a2a3a4a5 <linub7@gmail.com>
2022-10-20 10:10:37 +0330

    add navigation - make temp screens - add icons to bottom tab

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Text } from 'react-native';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
 import {
@@ -9,9 +10,26 @@ import {
   Lato_400Regular,
   Lato_700Bold,
 } from '@expo-google-fonts/lato';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import RestaurantsScreen from './src/features/restaurants/screens/restaurants.screen';
 import { theme } from './src/infrustructure/theme';
+import { SafeArea } from './src/components/utility/safe-area.component';
+
+const BottomTab = createBottomTabNavigator();
+
+const MapScreen = () => (
+  <SafeArea>
+    <Text>MapScreen</Text>
+  </SafeArea>
+);
+const SettingScreen = () => (
+  <SafeArea>
+    <Text>SettingScreen</Text>
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -29,7 +47,35 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <BottomTab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Restaurants') {
+                  iconName = focused ? 'restaurant' : 'restaurant-outline';
+                } else if (route.name === 'Map') {
+                  iconName = focused ? 'map' : 'map-outline';
+                } else if (route.name === 'Settings') {
+                  iconName = focused ? 'settings' : 'settings-outline';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#72BAFC',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
+            <BottomTab.Screen
+              name="Restaurants"
+              component={RestaurantsScreen}
+            />
+            <BottomTab.Screen name="Map" component={MapScreen} />
+            <BottomTab.Screen name="Settings" component={SettingScreen} />
+          </BottomTab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
